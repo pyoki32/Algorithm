d84b17d72ec77d98d586a9d901efb003af3aa4a6
Pyo <pyoki32@gmail.com>
2021-06-17 22:28:28 +0900

    modified solve

--- BaekJoon/src/Sol_1593.java
+++ BaekJoon/src/Sol_1593.java
@@ -8,15 +8,16 @@ public class Sol_1593 {
 	static int[] w_uppercase;
 	static int[] w_lowercase;
 	static ArrayList<Letter> wAl;
-	
-	static class Letter{
+
+	static class Letter {
 		char uLcase;
 		int idx;
 		int cnt;
-		public Letter(char uLcase,int idx, int cnt) {
-			this.uLcase=uLcase;
-			this.idx=idx;
-			this.cnt=cnt;
+
+		public Letter(char uLcase, int idx, int cnt) {
+			this.uLcase = uLcase;
+			this.idx = idx;
+			this.cnt = cnt;
 		}
 	}
 
@@ -34,7 +35,7 @@ public class Sol_1593 {
 		w_lowercase = new int[26];
 		for (int i = 0; i < g; i++) {
 			char w = W.charAt(i);
-			
+
 			if ('A' <= w && w <= 'Z') {
 				int w_idx = w - 'A';
 				w_uppercase[w_idx]++;
@@ -43,23 +44,21 @@ public class Sol_1593 {
 				w_lowercase[w_idx]++;
 			}
 		}
-		
-		wAl= new ArrayList<>();	
-		for(int i=0;i<26;i++) {
-			if(w_uppercase[i]!=0) {
-				wAl.add(new Letter('U',i,w_uppercase[i]));		
+
+		wAl = new ArrayList<>();
+		for (int i = 0; i < 26; i++) {
+			if (w_uppercase[i] != 0) {
+				wAl.add(new Letter('U', i, w_uppercase[i]));
 			}
-			if(w_lowercase[i]!=0) {
-				wAl.add(new Letter('L',i,w_lowercase[i]));		
-			}	
+			if (w_lowercase[i] != 0) {
+				wAl.add(new Letter('L', i, w_lowercase[i]));
+			}
 		}
-		
-		
-			
-		int [] s_uppercase = new int[26];
-		int [] s_lowercase = new int[26];
-		int answer=0;
-		//init
+
+		int[] s_uppercase = new int[26];
+		int[] s_lowercase = new int[26];
+		int answer = 0;
+		// init
 		for (int i = 0; i < g; i++) {
 			char s_temp = S.charAt(i);
 			if ('A' <= s_temp && s_temp <= 'Z') {
@@ -70,24 +69,23 @@ public class Sol_1593 {
 				s_lowercase[s_idx]++;
 			}
 		}
-		if(findMayaLetters(s_uppercase,s_lowercase)) answer++;
-		
-		
-		int start=0;
-		int end=g;
-		for(int i=start;i<s-g;i++) {
-			
-			//앞쪽 뺴기
+		if (findMayaLetters(s_uppercase, s_lowercase))
+			answer++;
+
+		int start = 0;
+		int end = g;
+		for (int i = start; i < s - g; i++) {
+			// 앞쪽 뺴기
 			char s_front = S.charAt(i);
 			if ('A' <= s_front && s_front <= 'Z') {
 				int s_idx = s_front - 'A';
-				
+
 				s_uppercase[s_idx]--;
 			} else {
 				int s_idx = s_front - 'a';
 				s_lowercase[s_idx]--;
 			}
-			//뒤쪽 더하기
+			// 뒤쪽 더하기
 			char s_back = S.charAt(end);
 			if ('A' <= s_back && s_back <= 'Z') {
 				int s_idx = s_back - 'A';
