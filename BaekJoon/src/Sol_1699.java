63607984406bd5f03db5ea9c867a396a45e9be2d
Pyo <pyoki32@gmail.com>
2021-07-17 17:16:22 +0900

    BaekJoon 1699 solve

---
+++ BaekJoon/src/Sol_1699.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Sol_1699 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int [] dp = new int [N+1];
+		for(int i=1;i<=N;i++)dp[i]=i;
+		
+		for(int i=2;i<=N;i++) {
+			for(int j=2;j*j<=i;j++) {
+				dp[i]=Math.min(dp[i], dp[i-j*j]+1);
+			}
+		}
+		System.out.println(dp[N]);
+		
+	}
+
+}
