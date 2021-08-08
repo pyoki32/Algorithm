51a8bf2ee19a664ac9568fd2a1026464e88c3c0e
pyoki32 <pyoki32@gmail.com>
2021-08-08 16:25:37 +0900

    programmers 정수_삼각형 solve

--- Programmers/src/Sol_정수_삼각형.java
+++ Programmers/src/Sol_정수_삼각형.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Sol_정수_삼각형 {
+public class Sol_정수_삼각형 {
+        public int solution(int[][] triangle) {
+            int answer = 0;
+            int [][] dp = new int[triangle.length][triangle.length];
+            dp[0][0]= triangle[0][0];
+            for(int i=1;i<triangle.length;i++){
+                for(int j = 0;j<=i;j++){
+                    if(j==0){
+                        dp[i][j]=dp[i-1][j]+triangle[i][j];
+                    }
+                    else if( j==i){
+                        dp[i][j]=dp[i-1][j-1]+triangle[i][j];
+                    }
+                    else{
+                        dp[i][j]=Math.max(dp[i-1][j-1],dp[i-1][j])+triangle[i][j];
+                    }
+                }
+            }
+            for(int i=0;i<triangle.length;i++){
+                answer = Math.max(dp[triangle.length-1][i],answer);
+            }
+            return answer;
+        }
 }
