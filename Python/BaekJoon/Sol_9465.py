9851e723e9b7cbded7247eb6a3280c52c50807dc
pyoki32 <pyoki32@gmail.com>
2021-09-15 20:14:17 +0900

    BaekJoon 9465 solve py

--- Python/BaekJoon/Sol_9465.py
+++ Python/BaekJoon/Sol_9465.py
@@ -2,20 +2,22 @@ T = int(input())
 
 for t in range(T):
     n = int(input())
-    arr = []
-    dp = [[0]* n for i in range(2)]
-    for i in range(2):
-        arr.append(list(map(int,input().split(' '))))
-    
-    dp[0][0]=arr[0][0]
-    dp[1][0]=arr[1][0]
-    dp[0][1]=dp[1][0]+arr[0][1]
-    dp[1][1]=dp[0][0]+arr[1][1]
-    
-    
-    for i in range(2, n):
-        for j in range(2):
-                dp[0][i]=max(dp[1][i-1],dp[1][i-2])+int(arr[0][i])
-                dp[1][i]=max(dp[0][i-1],dp[0][i-2])+int(arr[1][i])
 
-    print(max(dp[0][n-1],dp[1][n-1]))            
+    arr = []
+    dp = [[0] * n for i in range(2)]
+    for i in range(2):
+        arr.append(list(map(int, input().split(' '))))
+    
+    if n == 1:
+        print(max(arr[0][0], arr[1][0]))
+    else:
+        dp[0][0] = arr[0][0]
+        dp[1][0] = arr[1][0]
+        dp[0][1] = dp[1][0] + arr[0][1]
+        dp[1][1] = dp[0][0] + arr[1][1]
+
+        for i in range(2, n):
+            dp[0][i] = max(dp[1][i - 1], dp[1][i - 2]) + int(arr[0][i])
+            dp[1][i] = max(dp[0][i - 1], dp[0][i - 2]) + int(arr[1][i])
+
+        print(max(dp[0][n - 1], dp[1][n - 1]))
